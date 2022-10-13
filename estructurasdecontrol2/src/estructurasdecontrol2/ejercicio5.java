713c25f254ee0027dec43f150718019f5b4b28b1
oscaruda <ocaruda@gmail.com>
2022-10-13 11:46:20 +0200

    estructurasControl2

--- estructurasdecontrol2/src/estructurasdecontrol2/ejercicio5.java
+++ estructurasdecontrol2/src/estructurasdecontrol2/ejercicio5.java
@@ -2,9 +2,30 @@ package estructurasdecontrol2;
 
 public class ejercicio5 {
 	public static void main (String[]args) {
+		float altura = 1.75f;
+		boolean mujer = true;
 		
-	}
-	
-	
+		if (mujer = true ) {
+			if (altura < 1.35)
+				System.out.println("La mujer es enana");
+			else if(altura > 1.35 && altura < 1.65) {
+				System.out.println("La mujer es bajita");
+			}
+			else if (altura >1.65 && altura < 1.90) {
+				System.out.println("La mujer es alta");
+			}
+			else {
+				System.out.println("La mujer es gigante");
+			}
+		}
+			
+		
+		
+		}
+		
+		
+		
+		
+		
+		}
 
-}
